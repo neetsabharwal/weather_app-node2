36f1d4f49178cc7870a3d1ae962d1be01c00f0c5
Gurneet <gsingh2@atb.com>
2023-04-13 15:13:32 -0400

    added more data for weather

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ request({url, json: true}, (e,{body} = {})=>{
     } else if (body.error) {
         callback('Unable to find location!', undefined)
     } else {
-        callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.');
+        callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. Humidity is ' + body.current.humidity + '%.');
     }
 });
 };
